dd0a9fffaefba8b1d431fc9610c85252d38ca785
chrison.chai <chrison.chai@spreadtrum.com>
2020-04-22 10:10:32 +0800

    first commit

---
+++ sample/src/main/java/com/yalantis/phoenix/sample/PullToRefreshActivity.java
@@ -0,0 +1,68 @@
+package com.yalantis.phoenix.sample;
+
+import android.os.Bundle;
+import com.google.android.material.tabs.TabLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+/**
+ * Created by Oleksii Shliama.
+ */
+public class PullToRefreshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pull_to_refresh);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        viewPager.setAdapter(new SectionPagerAdapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public class SectionPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new ListViewFragment();
+                case 1:
+                default:
+                    return new RecyclerViewFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "ListView";
+                case 1:
+                default:
+                    return "RecyclerView";
+            }
+        }
+    }
+
+}
